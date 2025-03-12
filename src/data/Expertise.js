8c34e1267e561ef89b8e02409c0e23e7dd7fbf95
noel.buergler <noel.buergler@topix.ch>
2025-03-12 21:25:12 +0100

    updated

--- src/data/Expertise.js
+++ src/data/Expertise.js
@@ -5,17 +5,17 @@ export const Expertise = [
       { name: "German", level: "A2" },
       { name: "English", level: "C1" },
       { name: "Russian", level: "C2" },
-      { name: "Hebrew", level: "B2" },
+      { name: "Hebrew", level: "B1" },
     ],
   },
   {
     category: "IT & Programming",
     skills: [
-      { name: "SQL", level: "Advanced", details: ["MySQL", "MSSQL"] },
+      { name: "SQL", level: "Intermediate", details: ["MySQL", "MSSQL"] },
       { name: "Microsoft Office", level: "Advanced" },
       { name: "SAP S/4HANA", level: "Beginner" },
       { name: "Power BI", level: "Beginner" },
-      { name: "Camtasia", level: "Beginner" },
+      { name: "Camtasia", level: "Intermediate" },
     ],
   },
 ];
